42be4f0743231b7fcb0c76e1b4a78e3c2326e6fd
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-25 17:51:32 +0300

    connection ready to mongodb

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+const path = require('path')
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,6 +12,17 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-app.listen(4000, () => {
-  console.log('listening requests on port 4000');
-});
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(MONGODB_URI,
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+  .then(result => {
+    console.log('listening at port 4000')
+    app.listen(4000)
+  })
+  .catch(err => console.log(err))
+mongoose.connection.once('open', () => {
+  console.log('Connedted to mongoose db')
+}).catch((err) => {
+  throw new Error('Error with db', err)
+})
