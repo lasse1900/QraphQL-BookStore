df04763dedced6593ced768539dac651d4de3ad9
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-25 17:12:18 +0300

    GraphlQl Lists

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,8 +6,25 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
+
+// dummy data
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+];
+
+var authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' }
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -18,8 +35,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        console.log(parent)
-        return _.find(authors, { id: parent.authorId })
+        return _.find(authors, { id: parent.authorId });
       }
     }
   })
@@ -30,22 +46,15 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      }
+    }
   })
 });
-
-// dummy data
-var books = [
-  { name: 'Name of the wind', genre: 'Fantasy', id: '1', authorId: '1' },
-  { name: 'Howto play ball', genre: 'Sports', id: '2', authorId: '2' },
-  { name: 'Eleanor Roosevelt\'s time', genre: 'Histore', id: '3', authorId: '3' },
-];
-
-var authors = [
-  { name: 'Patric Rooney', age: 44, id: '1' },
-  { name: 'Tim Hill', age: 29, id: '2' },
-  { name: 'E.Roosevelt', age: 74, id: '3' },
-];
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -61,7 +70,7 @@ const RootQuery = new GraphQLObjectType({
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id })
+        return _.find(authors, { id: args.id });
       }
     }
   }
