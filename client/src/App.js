190eadfeca45ecf4b2a15a6c546674a871229fd1
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-26 09:55:12 +0300

    added Book List component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import BookList from './components/BookList';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Book List</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Book List</h1>
+        <BookList />
+      </div>
+    );
+  }
 }
 
 export default App;
